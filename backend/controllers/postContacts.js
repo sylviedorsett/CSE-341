172805907e5cd915828f64e8d333eeb8a1131e81
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-02 22:44:21 -0700

    formatting

--- backend/controllers/postContacts.js
+++ backend/controllers/postContacts.js
@@ -2,22 +2,27 @@
 const mongodb = require('../db/connect');
 
 const createContact = async (req, res) => {
-    //Create body to hold the data passed with the POST HTTP Request
-    const newContact = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-    };
-    //grab the collection
-    const data = await mongodb.getDb().db("PersonalAssignment_2").collection('contacts');
-    //Use the insertOne() method on the data to add the POST request
-    data.insertOne(newContact)
-    .then(result => {
-        res.sendStatus(201);
+  //Create body to hold the data passed with the POST HTTP Request
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  //grab the collection
+  const data = await mongodb.getDb().db('PersonalAssignment_2').collection('contacts');
+  //Use the insertOne() method on the data to add the POST request
+  data
+    .insertOne(newContact)
+    .then((result) => {
+      if (result.acknowledged) {
+        const id = result.insertedId;
+        console.log(id);
+      }
+      res.sendStatus(201);
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 };
 
-module.exports = {createContact};
+module.exports = { createContact };
