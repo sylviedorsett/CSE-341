576afd5f9c9a32f9ce4208718a12f87d958ae125
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-04 14:54:05 -0700

    Added Access-Control methods to app

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Import Express, MongoDB and Body-Parser
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("./backend/db/connect");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./backend/db/connect');
+const cors = require('cors');
 
 //Create a Port, ensure it directs to the environment variables OR the port you want
 const port = process.env.PORT || 3000;
@@ -16,17 +16,22 @@ app.use(bodyParser.json());
 
 //Set response header to '*' so it won't get blocked by CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Controll-Allow-Origin", "*");
+  res.setHeader('Access-Controll-Allow-Origin', '*');
+  res.setHeader(
+    'Acess-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, OPTIONS, DELETE');
   next();
 });
 
 //Direct the app to the routes entry point
-app.use("/", require("./backend/routes/index.js"));
+app.use('/', require('./backend/routes/index.js'));
 
 //Connect to the database
 mongodb.initDb((err, mongodb) => {
   if (err) {
-    console.log("Something went wrong.", err);
+    console.log('Something went wrong.', err);
   } else {
     app.listen(port);
     console.log(`Connected to DB.`);
