7c48b543335cf22337b819465ff0846f089aec81
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-07 10:16:55 -0700

    blank start

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../controllers');
-
-router.get('/', controller.returnSpouse);
-
-module.exports = router;
