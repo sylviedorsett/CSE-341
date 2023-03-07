e3bd8b325ac2e7ba07bd575dddab80c98aa7716c
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-07 13:13:11 -0700

    created swagger docs for api documentation

---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Personal Assignment 5",
+    description:
+      "An application to get, update, and delete College Course and Instructor information from a database.",
+  },
+  host: "localhost:3000",
+  schemes: ["https"],
+};
+
+const outputFile = "./swagger-output";
+const endpointsFiles = ["./routes/index.js"];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
