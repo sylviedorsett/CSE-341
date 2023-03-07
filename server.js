e1d8a78d517f5a3bbfc55c707bc19193b247b3f4
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-07 12:45:18 -0700

    rest file and server file completed

---
+++ server.js
@@ -0,0 +1,29 @@
+//Imports
+const express = requrie("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+
+const port = process.env.PORT || 3000;
+
+const server = express();
+
+server
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  })
+  .use("/", require("./routes/index"));
+
+mongodb.initDB((err, mongodb) => {
+  if (err) {
+    console.log("Something went wrong.", err);
+  } else {
+    server.listen(port);
+    console.log("Connected to database.");
+  }
+});
