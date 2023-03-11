d12cccb5f0184192ca95ba4b0e39c96b5adc5022
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-11 16:28:49 -0700

    fixed course route validation

--- routes/courses.js
+++ routes/courses.js
@@ -5,8 +5,8 @@ const validate = require("../middleware/validate");
 
 router.get("/", getCoursesController.getAllCourses);
 router.get("/:id", getCoursesController.getCourse);
-router.post("/", validate.saveCourse, getCoursesController.postCourse);
-router.put("/:id", validate.saveCourse, getCoursesController.putCourse);
+router.post("/", validate.newCourse, getCoursesController.postCourse);
+router.put("/:id", validate.newCourse, getCoursesController.putCourse);
 router.delete("/:id", getCoursesController.deleteCourse);
 
 module.exports = router;
