f74b6e41185113952455b0d42c63cf67b555f474
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-11 16:25:03 -0700

    validation

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 const getCoursesController = require("../controllers/courses");
+const validate = require("../middleware/validate");
 
 router.get("/", getCoursesController.getAllCourses);
 router.get("/:id", getCoursesController.getCourse);
-router.post("/", getCoursesController.postCourse);
-router.put("/:id", getCoursesController.putCourse);
+router.post("/", validate.saveCourse, getCoursesController.postCourse);
+router.put("/:id", validate.saveCourse, getCoursesController.putCourse);
 router.delete("/:id", getCoursesController.deleteCourse);
 
 module.exports = router;
