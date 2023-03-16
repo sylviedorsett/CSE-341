774fd37b73fa24eec38e73947225fa59e0f7e0b0
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-15 20:09:56 -0700

    courses.js routes added autho

--- routes/courses.js
+++ routes/courses.js
@@ -6,18 +6,8 @@ const auth = require("../middleware/authenticate.js");
 
 router.get("/", getCoursesController.getAllCourses);
 router.get("/:id", getCoursesController.getCourse);
-router.post(
-  "/",
-  auth.isAuthenticated,
-  validate.newCourse,
-  getCoursesController.postCourse
-);
-router.put(
-  "/:id",
-  auth.isAuthenticated,
-  validate.newCourse,
-  getCoursesController.putCourse
-);
+router.post("/", auth.isAuthenticated, validate.newCourse, getCoursesController.postCourse);
+router.put("/:id", auth.isAuthenticated, validate.newCourse, getCoursesController.putCourse);
 router.delete("/:id", getCoursesController.deleteCourse);
 
 module.exports = router;
