e0e0f988d43b47785db89aa734a094b4f52d93ce
Sylvie Dorsett <sylviejovet@hotmail.com>
2023-03-20 16:37:11 -0700

    added authenticate to get routes

--- routes/courses.js
+++ routes/courses.js
@@ -4,10 +4,20 @@ const getCoursesController = require("../controllers/courses");
 const validate = require("../middleware/validate");
 const auth = require("../middleware/authenticate.js");
 
-router.get("/", getCoursesController.getAllCourses);
-router.get("/:id", getCoursesController.getCourse);
-router.post("/", auth.isAuthenticated, validate.newCourse, getCoursesController.postCourse);
-router.put("/:id", auth.isAuthenticated, validate.newCourse, getCoursesController.putCourse);
+router.get("/", auth.isAuthenticated, getCoursesController.getAllCourses);
+router.get("/:id", auth.isAuthenticated, getCoursesController.getCourse);
+router.post(
+  "/",
+  auth.isAuthenticated,
+  validate.newCourse,
+  getCoursesController.postCourse
+);
+router.put(
+  "/:id",
+  auth.isAuthenticated,
+  validate.newCourse,
+  getCoursesController.putCourse
+);
 router.delete("/:id", getCoursesController.deleteCourse);
 
 module.exports = router;
--- routes/instructors.js
+++ routes/instructors.js
@@ -1,13 +1,31 @@
 const router = require("express").Router();
 
 const getInstructorsController = require("../controllers/instructors");
-const validate = require('../middleware/validate');
-const auth = require('../middleware/authenticate.js');
+const validate = require("../middleware/validate");
+const auth = require("../middleware/authenticate.js");
 
-router.get("/", getInstructorsController.getAllInstructors);
-router.get("/:id", getInstructorsController.getInstructor);
-router.post("/", auth.isAuthenticated, validate.newInstructor,getInstructorsController.postInstructor);
-router.put("/:id", auth.isAuthenticated, validate.newInstructor, getInstructorsController.putInstructor);
+router.get(
+  "/",
+  auth.isAuthenticated,
+  getInstructorsController.getAllInstructors
+);
+router.get(
+  "/:id",
+  auth.isAuthenticated,
+  getInstructorsController.getInstructor
+);
+router.post(
+  "/",
+  auth.isAuthenticated,
+  validate.newInstructor,
+  getInstructorsController.postInstructor
+);
+router.put(
+  "/:id",
+  auth.isAuthenticated,
+  validate.newInstructor,
+  getInstructorsController.putInstructor
+);
 router.delete("/:id", getInstructorsController.deleteInstructor);
 
 module.exports = router;
